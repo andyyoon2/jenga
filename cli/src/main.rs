b691d8c50bbfc50d42f3ae5bf63490a3d6c87720
Andy Yoon <783285+andyyoon2@users.noreply.github.com>
2026-05-26 22:12:29 -0700

    Simple command with jj shell-out

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    io::{self, Write},
+    process::Command,
+};
+
 use clap::{Parser, Subcommand};
 
 /// Submit your jj stack to GitHub.
@@ -16,12 +21,17 @@ enum Commands {
     Status {},
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = Args::parse();
 
     match &args.command {
         Commands::Status {} => {
-            println!("Welcome to jenga!");
+            println!("Welcome to jenga!\n");
+            let output = Command::new("jj").args(["log"]).output()?;
+            io::stdout().write_all(&output.stdout)?;
+            io::stderr().write_all(&output.stderr)?;
         }
     }
+
+    Ok(())
 }
