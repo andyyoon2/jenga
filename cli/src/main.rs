31e2ae9499607a8a492631ffb13851e2f9fcae19
Andy Yoon <783285+andyyoon2@users.noreply.github.com>
2026-05-26 22:06:32 -0700

    clap

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Parser, Subcommand};
+
+/// Submit your jj stack to GitHub.
+#[derive(Parser, Debug)]
+#[command(name = "jenga")]
+#[command(version, about, long_about = None)]
+#[command(arg_required_else_help = true)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// View GitHub status
+    Status {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match &args.command {
+        Commands::Status {} => {
+            println!("Welcome to jenga!");
+        }
+    }
 }
